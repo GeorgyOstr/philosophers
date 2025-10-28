16c96de5983170385da878c1b47b54324684dc53
George O <georgyostr@gmail.com>
2025-10-28 21:21:34 +0900

    some threaded stuff

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+
+void	*routine(void *arg)
+{
+	write(1, "hi\n", 3);
+	usleep(1000000);
+}
 
 int main(void)
 {
-	pid_t pid;
+	pthread_t		t1;
+	int				s;
+	pthread_attr_t	attr;
+	void	*res;
 
-	pid = fork();
-	if (!pid)
-	{
-		write(1, "hello\n", 6);
-	}
-	else
-	{
-		write(1, "bye!!\n", 6);
-	}
-	return 0;
+	s = pthread_attr_init(&attr);
+	s = pthread_create(&t1, &attr, routine, &s);
+	s = pthread_attr_destroy(&attr);
+	s = pthread_join(t1, &res);
+	printf("%d\n", s);
+	return (0);
 }
