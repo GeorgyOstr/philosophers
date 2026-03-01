1367edff8341c176aa115169d5c6d9755d801857
George O <georgyostr@gmail.com>
2026-03-02 01:14:28 +0900

    norm

--- routines.c
+++ routines.c
@@ -12,32 +12,31 @@
 
 #include "philosophers.h"
 
-long    get_time(void)
+long	get_time(void)
 {
-    struct timeval	tv;
+	struct timeval	tv;
 
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
 void	eat_routine(t_philo_info *philosopher)
 {
 	pthread_mutex_lock(philosopher->left_fork);
 	printf("%ld %d has taken a fork\n", get_time(), philosopher->thread_num);
-    pthread_mutex_lock(philosopher->right_fork);
+	pthread_mutex_lock(philosopher->right_fork);
 	printf("%ld %d has taken a fork\n", get_time(), philosopher->thread_num);
 	printf("%ld %d is eating\n", get_time(), philosopher->thread_num);
-    pthread_mutex_unlock(philosopher->left_fork);
+	pthread_mutex_unlock(philosopher->left_fork);
 	pthread_mutex_unlock(philosopher->right_fork);
 }
 
 void	sleep_routine(t_philo_info *philosopher)
 {
-    printf("%ld %d is sleeping\n", get_time(), philosopher->thread_num);
-    
+	printf("%ld %d is sleeping\n", get_time(), philosopher->thread_num);
 }
 
 void	think_routine(t_philo_info *philosopher)
 {
-    printf("%ld %d is thinking\n", get_time(), philosopher->thread_num);
+	printf("%ld %d is thinking\n", get_time(), philosopher->thread_num);
 }
