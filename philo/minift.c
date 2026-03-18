283c045afc35ab99e855f23d48591209f07150d6
Georgy Ostroumov <gostroum@c5r2s8.42tokyo.jp>
2026-03-18 19:34:43 +0900

    norm

--- philo/minift.c
+++ philo/minift.c
@@ -6,7 +6,7 @@
 /*   By: gostroum <gostroum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 15:21:27 by gostroum          #+#    #+#             */
-/*   Updated: 2026/02/24 15:49:14 by gostroum         ###   ########.fr       */
+/*   Updated: 2026/03/18 19:30:19 by gostroum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,12 +69,9 @@ int	parse_positive_int(char *str, int *out)
 	if (len == 20 || len == 0)
 		return (ATOI_ERROR);
 	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			return (ATOI_ERROR);
-		i++;
-	}
-	while (str[i] && i < 20)
+	while (str[i] && i < 15)
 	{
 		if ('0' > str[i] || str[i] > '9')
 			return (ATOI_ERROR);
